1d4521fc3ef2874a70c624e48271f9e97711cfd3
Renato Bento <webrenat@gmail.com>
2020-04-18 20:11:10 -0300

    Projeto semana oministack

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentController');
+const ProfilerController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfilerController.index);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+
+module.exports = routes;
